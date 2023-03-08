ec55b8176f0c010da252e3992033c29c82844469
Vicko Novianto <vicko1011@gmail.com>
2023-03-08 16:47:09 +0700

    chore: replace yarn with npm, install typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-require('dotenv').config();
-
-const listener = app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log(`Your app is listening on: ${listener.address().address}:${listener.address().port}`);
-})
