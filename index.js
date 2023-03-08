d900389ea46e67fd9041c009a6c5b9cb6f580370
Vicko Novianto <vicko1011@gmail.com>
2023-03-08 07:15:18 +0700

    chore: install express module

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
+
 require('dotenv').config();
 
+const listener = app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(`Your app is listening on: ${listener.address().address}:${listener.address().port}`);
+})
