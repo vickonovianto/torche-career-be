da21562afe49b02ccb285456316db989cd2e387c
Vicko Novianto <vicko1011@gmail.com>
2023-03-09 16:06:14 +0700

    chore: install cors middlware and add env type
    to prevent other site from calling the API

--- framework/express/app.ts
+++ framework/express/app.ts
@@ -1,8 +1,45 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
-const app: Express = express();
+// check validity of .env file
+function checkEnvFile() {
+    if (process.env.PORT === '') {
+        throw new Error('File .env: PORT cannot be empty');
+    }
+    if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'production') {
+        throw new Error('File .env: NODE_ENV can only contain development or production');
+    }
+    if (process.env.WHITELISTED_DOMAIN === '') {
+        throw new Error('File .env: WHITELISTED_DOMAIN cannot be empty');
+    }
+}
 
 function runServer() {
+    const app: Express = express();
+
+    // check validity of the .env file
+    checkEnvFile();
+
+    // set up cors middleware
+    const options: cors.CorsOptions = {
+        origin: function(origin, callback) {
+            if (process.env.NODE_ENV === "development") {
+                if (origin === process.env.WHITELISTED_DOMAIN || !origin) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            } else if (process.env.NODE_ENV === "production") {
+                if (origin === process.env.WHITELISTED_DOMAIN) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            }
+        }
+    };
+    app.use(cors(options));
+
     app.get('/', (req: Request, res: Response) => {
         res.send('Express + TypeScript Server');
     });
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 import runServer from './framework/express/app';
 
+// import environment variables from .env file
 dotenv.config();
+
+// run server
 runServer();
