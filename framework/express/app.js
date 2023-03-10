0d31f272bae0f0aa93e1d231ba03a13282bca0a8
Vicko Novianto <vicko1011@gmail.com>
2023-03-10 21:38:39 +0700

    chore: add host to .env and add thunder client config to git
    to simplify api testing with thunder client in VS Code

--- framework/express/app.js
+++ framework/express/app.js
@@ -7,14 +7,17 @@ const userController = require('./controller/user.js');
 
 // check validity of .env file
 function checkEnvFile() {
+    if (process.env.HOST === '') {
+        throw new Error('File .env: HOST cannot be empty');
+    }
     if (process.env.PORT === '') {
         throw new Error('File .env: PORT cannot be empty');
     }
     if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'production') {
         throw new Error('File .env: NODE_ENV can only contain development or production');
     }
-    if (process.env.WHITELISTED_DOMAIN === '') {
-        throw new Error('File .env: WHITELISTED_DOMAIN cannot be empty');
+    if (process.env.WHITELISTED_DOMAIN_CORS === '') {
+        throw new Error('File .env: WHITELISTED_DOMAIN_CORS cannot be empty');
     }
     if (process.env.MONGODB_URI === '') {
         throw new Error('File .env: MONGODB_URI cannot be empty');
@@ -42,13 +45,13 @@ function runServer() {
     const corsOptions = {
         origin: function(origin, callback) {
             if (process.env.NODE_ENV === "development") {
-                if (origin === process.env.WHITELISTED_DOMAIN || !origin) {
+                if (origin === process.env.WHITELISTED_DOMAIN_CORS || !origin) {
                     callback(null, true);
                 } else {
                     callback(new Error('Not allowed by CORS'));
                 }
             } else if (process.env.NODE_ENV === "production") {
-                if (origin === process.env.WHITELISTED_DOMAIN) {
+                if (origin === process.env.WHITELISTED_DOMAIN_CORS) {
                     callback(null, true);
                 } else {
                     callback(new Error('Not allowed by CORS'));
@@ -77,8 +80,9 @@ function runServer() {
     });
 
     const port = process.env.PORT;
-    app.listen(port, () => {
-        console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    const host = process.env.HOST;
+    app.listen(port, host, () => {
+        console.log(`⚡️[server]: Server is running at ${host}:${port}`);
     });
 }
 
